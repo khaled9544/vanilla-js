6304d5dcffd0a9a3861f85678fae9bd96bd43ef8
Khaled Ramadan <khaled.ra@live.com>
2023-05-31 21:20:44 +0300

    Homework enhancements and axios integration

--- app.js
+++ app.js
@@ -1,97 +1,55 @@
-function appendData(data) {
-    let mainContainer = document.getElementById("Posts");
-    let imageContainer = document.getElementById("images");
-    
-    
-    data.forEach(element => {
-        let div = document.createElement("div");
-        div.innerHTML =`${element.Resturant_id} resturant name ${element.Resturant_name} address ${element.Address}
-        email ${element.email} <img src=${element.image}>`;
-        let divimage=document.createElement("divimage");
-        mainContainer.appendChild(div);
-        imageContainer.appendChild(divimage);
-        
-        
-    });
-    
-    
-}
-const putPosts= () => {
-    
-fetch('https://jsonplaceholder.typicode.com/todos', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({"Resturant_id": 5,
-  "Resturant_name": "Barbar",
-  "Address": "Zaytouna Bey",
-  "email": "Info@Macdonalds.com",
-  "image":"https://sayajihotels.com/images/innerBanner/sayaji-indore-dining.jpg"
-})
-})
-
-  .then(response => response.json())
-  .then(response => console.log(JSON.stringify(response)))
-}
-putPosts();
-/*for (let i = 0; i < data.length; i++) {
+function appendData(restaurants) {
+  let mainContainer = document.getElementById("posts");
+  restaurants.forEach(element => {
     let div = document.createElement("div");
-    let divimage=document.createElement("images");
-    div.innerHTML =` Resturant id ${data[i].Resturant_id} resturant name ${data[i].Resturant_name} address ${data[i].Address}
-    email ${data[i].email}`;
-    divimage.innerHTML=data[i].image;
+    div.innerHTML = `${element.Resturant_id} resturant name ${element.Resturant_name} address ${element.Address}
+        email ${element.email} <img src=${element.image}>`;
     mainContainer.appendChild(div);
-    mainContainer.appendChild(divimage);
-}*/
+  });
+}
 
+function addRandomRestaurant(restaurants) {
+  const _restaurants = [...restaurants];
+  const newRestaurant = {
+      "Resturant_id": "khaled",
+      "Resturant_name": "l2",
+      "Address": "l2",
+      "email": "khaled@test.com",
+      "image": "random"
+  };
+  _restaurants.push(newRestaurant);
+  return _restaurants;
+};
 
-const getPosts= () => {
-    fetch('test.json', {
-        method: 'GET',
-        
+const putPosts = () => {
+  fetch('https://jsonplaceholder.typicode.com/todos', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "Resturant_id": 5,
+      "Resturant_name": "Barbar",
+      "Address": "Zaytouna Bey",
+      "email": "Info@Macdonalds.com",
+      "image": "https://sayajihotels.com/images/innerBanner/sayaji-indore-dining.jpg"
     })
-        .then((response) => response.json())
-        .then((json) => appendData(json));
+  })
+    .then(response => response.json())
+    .then(response => console.log(JSON.stringify(response)))
 }
+
+putPosts();
+
+const getPosts = () => {
+  fetch('test.json', {
+    method: 'GET',
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      const restaurants = addRandomRestaurant(json.restaurants);
+      appendData(restaurants);
+    });
+}
+
 getPosts();
-
-function check(form)
-{
-
- if(form.email.value == "walidhomsi50@gmail.com" && form.password.value == "P@ssword")
-  {
-    window.open('dashboard.html')
-  }
- else
- {
-   alert("Incorrect UserName or Bad Password")
-  }
-}
-/*const fs = require('fs');
-
-// Read the contents of the JSON file
-const data = fs.readFileSync('test.json');
-// Parse the JSON data into a JavaScript object
-const jsonData = JSON.parse(data);
-
-console.log("Before Adding data",JSON.stringify(jsonData, null, 4));
-
-// Modify the JavaScript object by adding new data
-jsonData.Resturants.push({
-    name: "James Den",
-    email: "james.den@example.com"
-});
-
-
-// Convert the JavaScript object back into a JSON string
-const jsonString = JSON.stringify(jsonData);
-
-fs.writeFileSync('test.json', jsonString, 'utf-8', (err) => {
-  if (err) throw err;
-  console.log('Data added to file');
-});
-
-const update_data = fs.readFileSync('test.json');
-const updated_jsonData = JSON.parse(update_data);
-console.log("After Adding data",JSON.stringify(updated_jsonData, null, 4));*/
---
+++ login.js
@@ -0,0 +1,37 @@
+// import axios from 'axios';
+// const axios = require('axios');
+
+
+const isLogin = sessionStorage.getItem("user");
+
+if (isLogin) {
+    window.location.replace('http://localhost:5500/dashboard.html');
+}
+
+const franceTimezone = 'Europe/Paris';
+const currentDate = new Date();
+const options = { timeZone: franceTimezone };
+const formattedDate = currentDate.toLocaleString('en-US', options);
+console.log('Current date and time in France:', formattedDate);
+
+// axios.get('/user?ID=12345').then(json=>console.log(json))
+
+// axios.get('')
+
+// axios.put( '', {  lkjflsd })
+
+    axios.get('https://jsonplaceholder.typicode.com/todos');
+
+
+
+
+function check(form) {
+    if (form.email.value == "123" && form.password.value == "123") {
+        // window.location.href = 'http://localhost:5500/dashboard.html';
+        window.location.replace('http://localhost:5500/dashboard.html');
+        sessionStorage.setItem("user", form.email.value)
+    }
+    else {
+        alert("Incorrect UserName or Bad Password");
+    }
+}
